3f971fdd7746b769ddf4c82714fe8acda6637b6b
baodzpro2009 <hoangthebaoyn2009284@gmail.com>
2026-02-04 10:37:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const wishes = [
   "May mắn gõ cửa, cơ hội đầy nhà 🍀"
 ];
 
-const RATE_WISH = 0.7;
+const RATE_WISH = 0.99;
 
 function randomGift(){
   return Math.random() < RATE_WISH
@@ -156,5 +156,6 @@ function createConfetti(){
   setTimeout(()=>c.remove(),9000);
 }
 setInterval(createConfetti,160);
+
 
 
